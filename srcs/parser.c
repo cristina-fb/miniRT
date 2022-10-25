16080a07321cb33055d9740acf766f77364bafd5
jalvarad-1 <jalvarad-1@github.com>
2022-10-25 17:44:11 +0200

    parsing cylinder

--- srcs/parser.c
+++ srcs/parser.c
@@ -6,7 +6,7 @@
 /*   By: jalvarad <jalvarad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 16:25:32 by jalvarad          #+#    #+#             */
-/*   Updated: 2022/10/19 18:16:29 by jalvarad         ###   ########.fr       */
+/*   Updated: 2022/10/25 17:42:58 by jalvarad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -216,6 +216,15 @@ bool parse_light(t_program *program)
 	return (true);
 }
 
+bool cylinder_basic_parse(t_program *program)
+{
+	return (ft_word_count(program->attr_buf[1], ',') != 3 || \
+		ft_word_count(program->attr_buf[2], ',') != 3 || \
+		!str_is_float(program->attr_buf[3]) || \
+		!str_is_float(program->attr_buf[4]) || \
+		ft_word_count(program->attr_buf[5], ',') != 3);
+}
+
 bool parse_cylinder(t_program *program)
 {
 	t_cylinder *cylinder;
@@ -225,7 +234,17 @@ bool parse_cylinder(t_program *program)
 	cylinder = ft_calloc(1, sizeof(t_cylinder));
 	if (!cylinder)
 		return (false);
-	*cylinder = (t_cylinder){NULL, NULL, 0.00, 0.00, 0.00, 0};
+	*cylinder = (t_cylinder){NULL, NULL, 0.00, 0.00, 0.00, NULL};
+	if (cylinder_basic_parse(program))
+	{
+		free(cylinder);
+		return (false);
+	}
+	cylinder->center = get_coords(program->attr_buf[1]);
+	cylinder->vector = orientation_vector(program->attr_buf[2]);
+	cylinder->diameter = ft_mod_atof(program->attr_buf[3], &err);
+	cylinder->height = ft_mod_atof(program->attr_buf[4], &err);
+	cylinder->rgb = get_rgb(program->attr_buf[5], &err);
 	
 	return (true);
 }
